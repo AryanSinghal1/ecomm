a2d291935b00c8ed799d9ba1c345611e3d66e665
Aryan <Aryans8791@gmail.com>
2021-10-23 18:33:59 +0530

    Added Responsiveness

--- src/components/Display.js
+++ src/components/Display.js
@@ -10,14 +10,14 @@ function Display() {
                 </div>
                 <div className="slshow">
                 <div className="slideshow">
-                     <div class="slide">
-                         <img class="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
+                     <div className="slide">
+                         <img className="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
                      </div> 
-                    <div class="slide">
-                        <img class="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
+                    <div className="slide">
+                        <img className="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
                     </div>
-                    <div class="slide">
-                        <img class="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
+                    <div className="slide">
+                        <img className="images" src="https://static.vecteezy.com/system/resources/previews/000/511/625/original/vector-sale-banner-big-summer-sale-sign-over-white-background.jpg" alt="img" />
                     </div>
                 </div>
             </div>
