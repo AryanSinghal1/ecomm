57fc723b7d27ba255561fb387a61134c0c87271b
Aryan <Aryans8791@gmail.com>
2021-10-30 20:30:57 +0530

    Authen

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,19 +4,19 @@ function Features() {
     return (
         <div className="features">
             <div className="spec">
-                <img className="specImage" src="https://th.bing.com/th/id/OIP.W3rWZKqwOEP-0fExg7rw9AHaGC?pid=ImgDet&rs=1"></img>
+                <img className="specImage" src="https://th.bing.com/th/id/OIP.W3rWZKqwOEP-0fExg7rw9AHaGC?pid=ImgDet&rs=1" alt="features"></img>
                 <div className="about">
                     <p>Free Delivery On Orders Above Rs. 500</p>
                 </div>
             </div>
             <div className="spec">
-            <img className="specImage" src="http://warrentruckandtrailerinc.com/files/2014/06/10off.jpg"></img>
+            <img className="specImage" src="http://warrentruckandtrailerinc.com/files/2014/06/10off.jpg" alt="features"></img>
                 <div className="about">
                     <p>10% Off On Payment with Bank's Debit/Credit Cards</p>
                 </div>
             </div>
             <div className="spec">
-            <img className="specImage" src="https://seeklogo.com/images/M/man-silhouette-delivery-logo-0DBA9FBE43-seeklogo.com.png"></img>
+            <img className="specImage" src="https://seeklogo.com/images/M/man-silhouette-delivery-logo-0DBA9FBE43-seeklogo.com.png" alt="features"></img>
                 <div className="about">
                     <p>Easy Returns/Replacements in 10 Days For You</p>
                 </div>
---
+++ src/components/Product.js
@@ -0,0 +1,39 @@
+const Product= [
+{
+    id: 1,
+    image: 'https://m.media-amazon.com/images/I/61TnX0PmqES._SL1500_.jpg',
+    heading: 'OnePlus Nord 2 5G',
+    about: 'OnePlus Nord 2 5G (Blue Haze, 8GB RAM, 128GB Storage)',
+    price:'1000'
+},
+{
+    id: 2,
+    image: 'https://m.media-amazon.com/images/I/81vDZyJQ-4L._SL1500_.jpg',
+    heading: 'Samsung Galaxy S20 FE 5G',
+    about: 'Samsung Galaxy S20 FE 5G (Cloud Navy, 8GB RAM, 128GB Storage)',
+    price:'1000'
+},
+{
+    id: 3,
+    image: 'https://m.media-amazon.com/images/I/71r69Y7BSeL._AC_SS450_.jpg',
+    heading: 'Samsung Galaxy M12 ',
+    about: 'Samsung Galaxy M12 (Blue,4GB RAM, 64GB Storage) 6000 mAh with 8nm Processor | True 48 MP Quad Camera | 90Hz Refresh Rate',
+    price:'1000'
+},
+{
+    id: 4,
+    image: 'https://m.media-amazon.com/images/I/71VQXjN6R7S._AC_SS450_.jpg',
+    heading: 'MI 11X 5G',
+    about: 'MI 11X 5G (Cosmic Black 8GB RAM 128GB ROM | SD 870 | DisplayMate A+ rated E4 AMOLED)',
+    price:'1000'
+},
+{
+    id: 5,
+    image: 'https://m.media-amazon.com/images/I/41rgQQmyj7L._AC_SR160,160_.jpg',
+    heading: 'Samsung Galaxy S21 5G',
+    about: 'Samsung Galaxy S21 5G (Phantom Gray, 8GB, 128GB Storage)',
+    price:'1000'
+},
+]
+
+export default Product;
